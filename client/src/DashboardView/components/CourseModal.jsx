96a13f66e68c74588a194b654d4c5c81592419da
MaikPython <soeoskarpaul@gmail.com>
2020-05-25 21:10:07 +0300

    commit

--- client/src/DashboardView/components/CourseModal.jsx
+++ client/src/DashboardView/components/CourseModal.jsx
@@ -8,7 +8,6 @@ const CourseModal = ({ modal, toggle }) => {
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
