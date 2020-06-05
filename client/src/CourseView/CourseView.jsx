b1664f99c572d8abb94bb339167e4ddc95de0f74
MaikPython <soeoskarpaul@gmail.com>
2020-06-05 10:10:07 +0300

    remove unnecessary variables

--- client/src/CourseView/CourseView.jsx
+++ client/src/CourseView/CourseView.jsx
@@ -1,32 +1,47 @@
-import React, { useState } from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import React, { useState } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import CourseViewModal from "./CourseViewModal";
 
 const data = [
-    {name: 'Hulgateooria', code: 'IF9120',  description: "hulgateooria sissejuhatus"},
-    {name: 'Interaktsioonidisain', code: 'IF1230',  description: "interaktsioonidisaini sissejuhatus"},
-    {name: 'Infosüsteemid', code: 'IFI9535', description: "infosüsteemide sissejuhatus"}
+  {
+    name: "Hulgateooria",
+    code: "IF9120",
+    description: "hulgateooria sissejuhatus",
+  },
+  {
+    name: "Interaktsioonidisain",
+    code: "IF1230",
+    description: "interaktsioonidisaini sissejuhatus",
+  },
+  {
+    name: "Infosüsteemid",
+    code: "IFI9535",
+    description: "infosüsteemide sissejuhatus",
+  },
 ];
 
 export const CourseView = () => {
-  const [isModalVisible, setModalVisible] = useState(false);
   const [openCourse, setOpenCourse] = useState(false);
   const toggle = (item) => setOpenCourse(item.code);
   return (
     <div>
       <h1>Course view</h1>
-      {
-        data.map(item => {
-            return (
-                <div>
-                    <CourseViewModal openKey={openCourse} toggleFn={toggle} data={item}/>
-                    <ListGroup onClick={() => toggle(item)} >
-                        <ListGroupItem>{item.name} {item.code} {item.description}</ListGroupItem>
-                    </ListGroup>
-                </div>
-            )
-        })
-      }
+      {data.map((item) => {
+        return (
+          <div>
+            <CourseViewModal
+              openKey={openCourse}
+              toggleFn={toggle}
+              data={item}
+            />
+            <ListGroup onClick={() => toggle(item)}>
+              <ListGroupItem>
+                {item.name} {item.code} {item.description}
+              </ListGroupItem>
+            </ListGroup>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- client/src/CourseView/CourseViewModal.jsx
+++ client/src/CourseView/CourseViewModal.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import {Modal, Button, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+import { Modal, Button, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 export const CourseViewModal = (props) => {
-    const { openKey, toggleFn, data } = props;
+  const { openKey, toggleFn, data } = props;
 
-    return (
-        <Modal isOpen={openKey === data.code}>
-            <ModalHeader >{data.name}</ModalHeader>
-            <ModalBody>
-                {data.description}
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" onClick={() => toggleFn(false)} >Close modal</Button>{' '}
-            </ModalFooter>
-        </Modal>
-    )
-
-}
+  return (
+    <Modal isOpen={openKey === data.code}>
+      <ModalHeader>{data.name}</ModalHeader>
+      <ModalBody>{data.description}</ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={() => toggleFn(false)}>
+          Tagase
+        </Button>{" "}
+      </ModalFooter>
+    </Modal>
+  );
+};
 
 export default CourseViewModal;
