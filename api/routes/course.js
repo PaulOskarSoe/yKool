7eb54eac5f9013a7f7ccb5729b95844ee369a911
MaikPython <soeoskarpaul@gmail.com>
2020-06-06 19:06:06 +0300

    get user courses

--- api/routes/course.js
+++ api/routes/course.js
@@ -11,11 +11,21 @@ router.post("/new_course", async (req, res) => {
     return res.json({ message: "Missing fields: [code, name]", code: 403 });
 
   // check if user is teacher
-  const isTeahcer = await User.findOne({ _id: userId });
-  if (isTeahcer && isTeahcer.role !== 1) {
+  const teacher = await User.findOne({ _id: userId });
+
+  if (teacher && teacher.role !== 1) {
     return res.json({ message: "User has to be a teacher", code: 401 });
   }
+
   const newCourse = new Course({ name, code, description });
+  try {
+    if (newCourse) {
+      await User.updateMany(
+        { _id: teacher._id },
+        { $addToSet: { courseID: newCourse._id } }
+      );
+    }
+  } catch (error) {}
   if (newCourse) {
     newCourse
       .save()
@@ -33,6 +43,31 @@ router.post("/new_course", async (req, res) => {
           code: 400,
         });
       });
+  }
+});
+
+// we map all user courseIDs
+// we get all course objects by courseIDs, which are attached to user object
+
+router.get(`/teacher/:userId`, async (req, res) => {
+  if (!req.user) return res.json({ message: "Needs auth", code: 401 });
+  try {
+    const user = await User.findById(req.params.userId);
+    const courses = await Course.find({ _id: { $in: user.courseID } });
+    return res.json({ data: courses, code: 200 });
+  } catch (error) {
+    return res.json({ error, code: 403 });
+  }
+});
+
+router.get(`/student/:userId`, async (req, res) => {
+  if (!req.user) return res.json({ message: "Needs auth", code: 401 });
+  try {
+    const user = await User.findById(req.params.userId);
+    const courses = await Course.find({ _id: { $in: user.courseID } });
+    return res.json({ data: courses, code: 200 });
+  } catch (error) {
+    return res.json({ error, code: 403 });
   }
 });
 
