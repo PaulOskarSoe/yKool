5f531ea286022980dfa9c4c40c23afb8af87bd93
MaikPython <soeoskarpaul@gmail.com>
2020-06-13 18:47:33 +0300

    removing course added

--- client/src/CourseView/CourseView.jsx
+++ client/src/CourseView/CourseView.jsx
@@ -8,11 +8,16 @@ import CourseFormModal from "./CourseFormModal";
 
 import { Button } from "reactstrap";
 import { UserContext } from "./../store/UserContextProvider";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 export const CourseView = () => {
   const { user } = useContext(UserContext);
   const [courses, setCourses] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [openCourse, setOpenCourse] = useState(false);
+
+  const toggle = (item) => setOpenCourse(item.code);
 
   useEffect(() => {
     async function fetchData() {
@@ -29,12 +34,21 @@ export const CourseView = () => {
       setCourses([]);
     };
   }, []);
-  const [openCourse, setOpenCourse] = useState(false);
-  const toggle = (item) => setOpenCourse(item.code);
-  console.log("courses:", courses);
+
+  const deleteCourse = async (id) => {
+    try {
+      const response = await axios.delete(`/api/v1/courses/${id}`);
+      if (response && response.status === 200) {
+        const newArray = courses.filter((course) => course._id !== id);
+        setCourses(newArray);
+      }
+    } catch (error) {
+      console.log("Error while removing a course", error.response);
+    }
+  };
+
   return (
     <div>
-      <h1>Course view</h1>
       <Button color="primary" onClick={() => setModalVisible(true)}>
         Lisa kursus
       </Button>
@@ -52,9 +66,38 @@ export const CourseView = () => {
                 toggleFn={toggle}
                 data={item}
               />
-              <ListGroup onClick={() => toggle(item)}>
+              <ListGroup>
                 <ListGroupItem>
-                  {item.name} {item.code} {item.description}
+                  <label onClick={() => toggle(item)} style={{ width: "100%" }}>
+                    {item.name} {item.code} {item.description}
+                  </label>
+
+                  {user.role === 1 && (
+                    <FaTrashAlt
+                      style={{ float: "right" }}
+                      onClick={() =>
+                        Swal.fire({
+                          title: "Oled kindel?",
+                          text:
+                            "Kas oled kindel, et soovid seda kursust eemaldada?",
+                          icon: "warning",
+                          showCancelButton: true,
+                          confirmButtonColor: "#3085d6",
+                          cancelButtonColor: "#d33",
+                          confirmButtonText: "Jah, kustuta!",
+                        }).then((result) => {
+                          deleteCourse(item._id);
+                          if (result.value) {
+                            Swal.fire(
+                              "Kustutatud!",
+                              "Valitud kursus on kustutatud.",
+                              "success"
+                            );
+                          }
+                        })
+                      }
+                    />
+                  )}
                 </ListGroupItem>
               </ListGroup>
             </div>
