3c699b7e9ae002efa582be9f148506488e2be0c7
MaikPython <soeoskarpaul@gmail.com>
2020-06-15 09:35:22 +0300

    fix submission modal

--- api/models/Submission.js
+++ api/models/Submission.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const submissionSchema = new mongoose.Schema({
   userID: { type: String, required: true },
   createdAt: { type: Date.now(), required: true },
+  createdAt: { type: Date, default: Date.now },
+  isAccepted: { type: Boolean, required: true, default: false },
 });
 
 const Submission = mongoose.model("submission", submissionSchema);
