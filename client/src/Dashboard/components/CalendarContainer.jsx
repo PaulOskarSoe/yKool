0f759146b11dc4cd7081ab90516df35d247e631d
Daniel Gurevits <43746225+mitsukiz@users.noreply.github.com>
2020-06-16 18:11:17 +0300

    Add TimeLine

--- client/src/Dashboard/components/CalendarContainer.jsx
+++ client/src/Dashboard/components/CalendarContainer.jsx
@@ -1,17 +1,58 @@
 import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import Timeline from 'react-calendar-timeline'
+import 'react-calendar-timeline/lib/Timeline.css'
+import moment from 'moment'
+
 
 const CalendarContainer = () => {
-  const [date, updateDate] = useState(new Date());
+const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+ 
+const items = [
+  {
+    id: 1,
+    group: 1,
+    title: 'item 1',
+    start_time: moment(),
+    end_time: moment().add(1, 'hour'),
+    itemProps: {
+      // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+      'data-custom-attribute': 'Random content',
+      'aria-hidden': true,
+      onDoubleClick: () => { console.log('You clicked double!') },
+      className: 'weekend',
+      style: {
+        background: 'fuchsia'
+      }
+    }
+  },
+  {
+    id: 2,
+    group: 2,
+    title: 'item 2',
+    start_time: moment().add(-0.5, 'hour'),
+    end_time: moment().add(0.5, 'hour')
+  },
+  {
+    id: 3,
+    group: 1,
+    title: 'item 3',
+    start_time: moment().add(2, 'hour'),
+    end_time: moment().add(3, 'hour')
+  }
 
-  const onChange = (newDate) => updateDate(newDate);
+]
 
-  return (
-    <div className="calendar-container">
-      <Calendar onChange={onChange} value={date} />
+return (
+  <div className="calendar-container">
+  <Timeline
+      groups={groups}
+      items={items}
+      defaultTimeStart={moment().add(-12, 'hour')}
+      defaultTimeEnd={moment().add(12, 'hour')}
+    />
     </div>
-  );
+)
+
 };
 
 export default CalendarContainer;
