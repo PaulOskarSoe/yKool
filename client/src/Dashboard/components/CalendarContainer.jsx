24037831dbc4506f40517c5ad04543d9f7f1c780
Daniel Gurevits <43746225+mitsukiz@users.noreply.github.com>
2020-06-17 19:21:39 +0300

    TimeLine styles

--- client/src/Dashboard/components/CalendarContainer.jsx
+++ client/src/Dashboard/components/CalendarContainer.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Timeline from 'react-calendar-timeline'
 import 'react-calendar-timeline/lib/Timeline.css'
 import moment from 'moment'
+import "moment/locale/et"
 
 
 const CalendarContainer = () => {
-const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }]
+const groups = [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }, { id: 3, title: 'group 3' }]
  
 const items = [
   {
@@ -13,7 +14,7 @@ const items = [
     group: 1,
     title: 'item 1',
     start_time: moment(),
-    end_time: moment().add(1, 'hour'),
+    end_time: moment().add(3, 'day'),
     itemProps: {
       // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
       'data-custom-attribute': 'Random content',
@@ -29,16 +30,36 @@ const items = [
     id: 2,
     group: 2,
     title: 'item 2',
-    start_time: moment().add(-0.5, 'hour'),
-    end_time: moment().add(0.5, 'hour')
+    start_time: moment(),
+    end_time: moment().add(1, 'day'),
+    itemProps: {
+      // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+      'data-custom-attribute': 'Random content',
+      'aria-hidden': true,
+      onDoubleClick: () => { console.log('You clicked double!') },
+      className: 'weekend',
+      style: {
+        background: 'fuchsia'
+      }
+    }
   },
   {
     id: 3,
-    group: 1,
+    group: 3,
     title: 'item 3',
-    start_time: moment().add(2, 'hour'),
-    end_time: moment().add(3, 'hour')
-  }
+    start_time: moment(),
+    end_time: moment().add(2, 'day'),
+    itemProps: {
+      // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+      'data-custom-attribute': 'Random content',
+      'aria-hidden': true,
+      onDoubleClick: () => { console.log('You clicked double!') },
+      className: 'weekend',
+      style: {
+        background: 'fuchsia'
+      }
+    }
+  },
 
 ]
 
@@ -47,8 +68,10 @@ return (
   <Timeline
       groups={groups}
       items={items}
-      defaultTimeStart={moment().add(-12, 'hour')}
-      defaultTimeEnd={moment().add(12, 'hour')}
+      defaultTimeStart={moment().add(-3, 'day')}
+      defaultTimeEnd={moment().add(4, 'day')}
+      lineHeight={80}
+      //onItemClick={}
     />
     </div>
 )
